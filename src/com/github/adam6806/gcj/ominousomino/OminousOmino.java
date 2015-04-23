e8fd811124e52680eb5cb1268fb185e66b1e598b
adam.smith <adam.smith@RTASmith04.RecondoTech.com>
2015-04-23 07:53:50 -0600

    Updated progress

--- src/com/github/adam6806/gcj/ominousomino/OminousOmino.java
+++ src/com/github/adam6806/gcj/ominousomino/OminousOmino.java
@@ -5,13 +5,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Created by Adam on 4/21/2015.
  */
 public class OminousOmino {
-    private static final String FILE_NAME = "D-small";
+    private static final String FILE_NAME_LETTER = "D";
+    private static final String FILE_NAME_SIZE = "small";
+    private static final String FILE_NAME = FILE_NAME_LETTER + "-" + FILE_NAME_SIZE;
     private static final URL FILE_PATH = OminousOmino.class.getResource(FILE_NAME + "-practice.in");
     private static final File INPUT_FILE = new File(FILE_PATH.getPath());
     private static final File OUTPUT_FILE = new File(FILE_PATH.getPath() + ".out");
@@ -33,14 +38,43 @@ public class OminousOmino {
         String line = scanner.nextLine();
         String[] array = line.split(" ");
         System.out.println(line);
-        int size = Integer.parseInt(array[0]);
-        int row = Integer.parseInt(array[1]);
-        int column = Integer.parseInt(array[2]);
+        int k = Integer.parseInt(array[0]);
+        int n = Integer.parseInt(array[1]);
+        int m = Integer.parseInt(array[2]);
         String result = "";
-        if (size * 2 <= (row * column) && (row * column) % size == 0) {
+        //Normalize where n>=m always
+        if(m>n) {
+            int x = n;
+            n = m;
+            m = x;
+        }
+        if(k==1) {
             result = GABRIEL;
+        } else if(m==1) {
+            if((n==1 && k>=2) || (n==3 && k>=2) || (n>3 && k>=3)) {
+                result = RICHARD;
+            } else {
+                result = GABRIEL;
+            }
         } else {
-            result = RICHARD;
+            Set<Integer> richardWinsIndexes = new HashSet();
+            for (int i = 2; i < k; i++) {
+                if(m>i) {
+                    if(m%i!=0) richardWinsIndexes.add(i);
+                } else {
+                    if(i%m!=0) richardWinsIndexes.add(i);
+                }
+                if(n>i) {
+                    if(n%i!=0) richardWinsIndexes.add(i);
+                } else {
+                    if(i%n!=0) richardWinsIndexes.add(i);
+                }
+            }
+            if(richardWinsIndexes.contains(k)) {
+                result = RICHARD;
+            } else {
+                result = GABRIEL;
+            }
         }
         return result;
     }
